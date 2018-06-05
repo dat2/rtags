1df42ab538e9d480c0cf0e742934e9dcf997d70a
Nicholas Dujay <nickdujay@gmail.com>
2018-06-04 20:30:45 -0400

    create tag data structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::fmt;
+
+#[derive(Debug)]
+enum ExCmd {
+    RegexPattern(String),
+    QPattern(String),
+    LineNo(usize),
+}
+
+impl fmt::Display for ExCmd {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ExCmd::RegexPattern(p) => write!(f, "/{}/;\"", p),
+            ExCmd::QPattern(p) => write!(f, "?{}?", p),
+            ExCmd::LineNo(line) => write!(f, "{}", line),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Tag {
+    name: String,
+    filename: String,
+    excmd: ExCmd,
+}
+
+impl fmt::Display for Tag {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}\t{}\t{}", self.name, self.filename, self.excmd)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
